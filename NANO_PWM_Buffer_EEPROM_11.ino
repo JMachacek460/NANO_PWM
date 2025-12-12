16e04bb9e8423d1042bf36534c67da6c96e7bda4
Jaroslav Macháček <jmachacek460@gmail.com>
2025-12-12 14:35:46 +0100

    pridan Decimal a Columns Separatory a dále příkaz *RST

--- NANO_PWM_Buffer_EEPROM_11.ino
+++ NANO_PWM_Buffer_EEPROM_11.ino
@@ -16,7 +16,7 @@
 
 
 #define VERSION_SIZE 5
-#define VERSION "V1.2"
+#define VERSION "V1.3"
 
 // If you uncomment the following line, you will activate debug mode
 //#define DEBUG_MODE 
@@ -34,13 +34,19 @@ const char TEXT_S[] PROGMEM = "-s [number1] [number2] to set limits for correct 
 const char TEXT_E[] PROGMEM = "-e [number1] to set the number of consecutive errors before the error pin (0-255) pin12 flips.";
 const char TEXT_TE[] PROGMEM = "-te [number] minimum error signaling duration (10-65000) ms.";
 const char TEXT_BPS[] PROGMEM = "-b [number] Serial buat rate 96 -> 9600, 144 -> 14400, 192 -> 19200, 288 -> 28800, 384 -> 38400, 576 -> 57600, 1152 -> 115200";
-const char TEXT_L[] PROGMEM = "-l [number] 0/1 - no/yes lists current values of frequency and duty cycle";
+const char TEXT_L[] PROGMEM = "-l [number] 0/1 - no/yes lists current values of frequency and duty cycle.";
 const char TEXT_H[] PROGMEM = "-h for help\n";
 const char TEXT_IDN[] PROGMEM = "Arduino NANO for measuring PWM signal duty cycle.";
-const char TEXT_HIDN[] PROGMEM = "*IDN?";
-const char TEXT_FETC[] PROGMEM = ":FETCh? returns the duty cycle values ​​of the PWM signal in per mille";
-const char TEXT_PWID[] PROGMEM = ":MEASure:PWIDth? returns the length value of the HIGH signal";
-const char TEXT_PER[] PROGMEM = ":MEASure:PERiod? returns the signal period value";
+const char TEXT_HIDN[] PROGMEM = "*IDN? returns IDN";
+const char TEXT_RST[] PROGMEM = "*RST sets all parameters to factory settings.";
+const char TEXT_FETC[] PROGMEM = ":FETCh? returns the duty cycle values ​​of the PWM signal in per mille.";
+const char TEXT_PWID[] PROGMEM = ":MEASure:PWIDth? returns the length value of the HIGH signal.";
+const char TEXT_PER[] PROGMEM = ":MEASure:PERiod? returns the signal period value.";
+const char TEXT_DS[] PROGMEM = "-ds [char] decimal separator.";
+const char TEXT_CS[] PROGMEM = "-cs [char] columns separator.";
+
+
+
 
 #define ERROR_OFF 255
 #define MAX_COMMAND_LENGTH 60 // Max. délka příkazu pro char buffer
@@ -71,7 +77,8 @@ unsigned long impuls;        // delka impulsu v us
 float pwid = 0.0;            // delka impulsu v sekundách
 char inChar;                 // po čtení seriové linky
 const char *cmd;             // ukazatel na incomingBuffer
-bool precteno;               // zda byl dekodován nějaký příkaz
+byte precteno;               // 0 - nic se zatím nenačetko; 2 - byl načten příkaz pro měření; 3 - byl vytisknut poslední přikaz pro měřeni; 255-ukončit while
+//bool nacteno;                // zda byl dekodován nějaký příkaz
 char incomingBuffer[MAX_COMMAND_LENGTH];
 byte bufferIndex = 0; 
 bool newData = false;
@@ -142,6 +149,8 @@ struct MojeNastaveni {
   unsigned int t_error;    // minimum error signaling duration ms
   unsigned int bps;        // Serial buat rate
   byte listing;            // 0/1 - no/yes lists current values of frequency and duty cycle 
+  char decimalSeparator;
+  char columnsSeparator;   // 
 };
 
 // Global instance of our structure, which we will work with in the program
@@ -169,7 +178,29 @@ void zobrazNastaveni() {
   Serial.print(F("Min error signaling duration: ")); Serial.print(aktualniNastaveni.t_error); Serial.println(F(" ms"));
   Serial.print(F("Serial line speed: ")); Serial.print(aktualniNastaveni.bps); Serial.println(F("00 baud"));
   Serial.print(F("Listing of measured frequency and duty cycle values: ")); Serial.print(aktualniNastaveni.listing); Serial.println(F("[0-No; 1-Yes]"));
+  Serial.print(F("Decimal separator: '")); Serial.print(aktualniNastaveni.decimalSeparator); Serial.println(F("'"));
+  Serial.print(F("Columns separator: '")); Serial.print(aktualniNastaveni.columnsSeparator); Serial.println(F("'"));
 }
+
+void tovarniNastaveni(){
+    strncpy(aktualniNastaveni.verze, VERSION, VERSION_SIZE);
+    aktualniNastaveni.spodni_hranice=40;
+    aktualniNastaveni.horni_hranice=60;
+    aktualniNastaveni.input=0;
+    aktualniNastaveni.min_perioda=18000;
+    aktualniNastaveni.max_perioda=22000;
+    aktualniNastaveni.min_strida=280;
+    aktualniNastaveni.max_strida=330;
+    aktualniNastaveni.max_error=1;
+    aktualniNastaveni.t_error=800;
+    aktualniNastaveni.bps=96;
+    aktualniNastaveni.listing=0;
+    aktualniNastaveni.decimalSeparator=',';
+    aktualniNastaveni.columnsSeparator=';';
+  
+    EEPROM.put(EEPROM_ADRESA, aktualniNastaveni);
+}
+
 
 void setup() {
   pinMode(INPUT_PIN, INPUT);
@@ -185,20 +216,7 @@ void setup() {
   if (strcmp(aktualniNastaveni.verze, VERSION) != 0){
     Serial.begin(9600); // Dočasné
     Serial.println(F("First run !!!!"));
-    strncpy(aktualniNastaveni.verze, VERSION, VERSION_SIZE);
-    aktualniNastaveni.spodni_hranice=40;
-    aktualniNastaveni.horni_hranice=60;
-    aktualniNastaveni.input=0;
-    aktualniNastaveni.min_perioda=18000;
-    aktualniNastaveni.max_perioda=22000;
-    aktualniNastaveni.min_strida=280;
-    aktualniNastaveni.max_strida=330;
-    aktualniNastaveni.max_error=1;
-    aktualniNastaveni.t_error=800;
-    aktualniNastaveni.bps=96;
-    aktualniNastaveni.listing=0;
-  
-    EEPROM.put(EEPROM_ADRESA, aktualniNastaveni);
+    tovarniNastaveni();
   }
   Serial.begin(aktualniNastaveni.bps * 100UL);
   Serial.println(F("\n\nThe Arduino is ready to evaluate the rectangular signal on PIN 2. It will evaluate its period and duty cycle."));
@@ -366,110 +384,160 @@ void loop() {
   if (newData) {
     // --- Používáme C-řetězce a strcmp/strncmp místo String.equals/startsWith ---
     cmd = incomingBuffer;
-    precteno = false;
-    if (strcmp(cmd, "-h") == 0) {
-      tiskniProgmem(TEXT_T);
-      tiskniProgmem(TEXT_I);
-      tiskniProgmem(TEXT_P);
-      tiskniProgmem(TEXT_S);
-      tiskniProgmem(TEXT_E);
-      tiskniProgmem(TEXT_TE);
-      tiskniProgmem(TEXT_BPS);
-      tiskniProgmem(TEXT_L);
-      tiskniProgmem(TEXT_HIDN);
-      tiskniProgmem(TEXT_FETC);
-      tiskniProgmem(TEXT_PWID);
-      tiskniProgmem(TEXT_PER);
-      precteno = true;
-    } 
-    if (strncmp(cmd, "*idn?", 5) == 0){
-      //*IDN?
-      Serial.print(reinterpret_cast<const __FlashStringHelper *>(TEXT_IDN)); Serial.print(F(" Version: ")); Serial.println(aktualniNastaveni.verze);
-      precteno = true;
-    }
-    if (strncmp(cmd, ":fetch?", 7) == 0 ){
-      //:FETCh?
-      Serial.println(stableDutyCyclePromile);
-      if (strlen(cmd) > 8) cmd = cmd + 8;
-      else precteno = true;
-    }
-    if (strncmp(cmd, ":fetc?", 6) == 0 ){
-      //:FETCh?
-      Serial.println(stableDutyCyclePromile);
-      if (strlen(cmd) > 7) cmd = cmd + 7;
-      else precteno = true;
-    }
-    if (strncmp(cmd, ":measure:pwidth?",16) == 0 ){
-      //:MEASure:PWIDth?
-      pwid = (float)impuls / 1000000.0;
-      Serial.println(pwid,6);
-      if (strlen(cmd) > 17) cmd = cmd + 17;
-      else precteno = true;
-    }
-    if (strncmp(cmd, ":meas:pwid?", 11) == 0 ){
-      //:MEASure:PWIDth?
-      pwid = (float)impuls / 1000000.0;
-      Serial.println(pwid,6);
-      if (strlen(cmd) > 12) cmd = cmd + 12;
-      else precteno = true;
-    }
-    if (strncmp(cmd, ":measure:period?", 16) == 0 ){
-      //MEASure:PERiod?
-      Serial.println(stablePeriod / 1000000.0,6);
-      //if (strlen(cmd) > 17) cmd = cmd + 17;
-      precteno = true;
-    }
-    if (strncmp(cmd, ":meas:per?", 10) == 0 ){
-      //MEASure:PERiod?
-      Serial.println(stablePeriod / 1000000.0,6);
-      //if (strlen(cmd) > 11) cmd = cmd + 11;
-      precteno = true;
-    }
-    if (strncmp(cmd, "-te", 3) == 0) {
-      if (! zpracujUniverzalniPrikaz(cmd, 100, 65000, nullptr, nullptr, &aktualniNastaveni.t_error,  nullptr)){
-        tiskniProgmem(TEXT_TE);
-      }; precteno = true;
-    } 
-    if (strncmp(cmd, "-t ", 3) == 0) {
-      if (! zpracujUniverzalniPrikaz(cmd, 1, 99, &aktualniNastaveni.spodni_hranice, &aktualniNastaveni.horni_hranice)){
+    precteno = 0; 
+    while ( precteno!=255){
+      if (strcmp(cmd, "-h") == 0) {
         tiskniProgmem(TEXT_T);
-      }; precteno = true;
-    } 
-    if (strncmp(cmd, "-i", 2) == 0) {
-      if (! zpracujUniverzalniPrikaz(cmd, 0, 1, &aktualniNastaveni.input, nullptr, nullptr, nullptr)){
         tiskniProgmem(TEXT_I);
-      }; precteno = true;
-    } 
-    if (strncmp(cmd, "-l", 2) == 0) {
-      if (! zpracujUniverzalniPrikaz(cmd, 0, 1, &aktualniNastaveni.listing, nullptr, nullptr, nullptr)){
-        tiskniProgmem(TEXT_L);
-      }; precteno = true;
-    }
-    if (strncmp(cmd, "-p", 2) == 0) {
-      if (! zpracujUniverzalniPrikaz(cmd, 100, 65000, nullptr, nullptr, &aktualniNastaveni.min_perioda, &aktualniNastaveni.max_perioda)){
         tiskniProgmem(TEXT_P);
-      }; precteno = true;
-    } 
-    if (strncmp(cmd, "-s", 2) == 0) {
-      if (! zpracujUniverzalniPrikaz(cmd, 1, 499, nullptr, nullptr, &aktualniNastaveni.min_strida, &aktualniNastaveni.max_strida)){
         tiskniProgmem(TEXT_S);
-      }; precteno = true;
-    } 
-    if (strncmp(cmd, "-e", 2) == 0) {
-      if (! zpracujUniverzalniPrikaz(cmd, 0, 255, &aktualniNastaveni.max_error, nullptr, nullptr, nullptr)){
         tiskniProgmem(TEXT_E);
-      }; precteno = true;
-    } 
-    if (strncmp(cmd, "-b", 2) == 0) {
-      if (! zpracujUniverzalniPrikaz(cmd, 96, 1152, nullptr, nullptr, &aktualniNastaveni.bps,  nullptr)){
+        tiskniProgmem(TEXT_TE);
         tiskniProgmem(TEXT_BPS);
-      }; precteno = true;
-    } 
-    if (!precteno){
-      Serial.print(F("Unknown command or wrong format: "));
-      Serial.println(cmd);
-      tiskniProgmem(TEXT_H);
+        tiskniProgmem(TEXT_L);
+        tiskniProgmem(TEXT_DS);
+        tiskniProgmem(TEXT_CS);
+        tiskniProgmem(TEXT_HIDN);
+        tiskniProgmem(TEXT_FETC);
+        tiskniProgmem(TEXT_PWID);
+        tiskniProgmem(TEXT_PER);
+        
+        precteno = 255;
+      } 
+      if (strncmp(cmd, "*idn?", 5) == 0){
+        //*IDN?
+        Serial.print(reinterpret_cast<const __FlashStringHelper *>(TEXT_IDN)); Serial.print(F(" Version: ")); Serial.println(aktualniNastaveni.verze);
+        precteno = 255;
+      }
+      if (strncmp(cmd, "*rst", 4) == 0){
+        //*RST
+        Serial.print(reinterpret_cast<const __FlashStringHelper *>(TEXT_RST)); Serial.print(F(" Version: ")); Serial.println(aktualniNastaveni.verze);
+        tovarniNastaveni();
+        zobrazNastaveni();
+        precteno = 255;
+      }
+      if (strncmp(cmd, ":fetch?", 7) == 0 ){
+        //:FETCh?
+        if(precteno==2 || precteno==3){ Serial.print(aktualniNastaveni.columnsSeparator);Serial.print(' ');}
+        Serial.println(stableDutyCyclePromile);
+        if (strlen(cmd) > 8) { cmd += 8; precteno = 2;}
+        else precteno = 4;
+      }
+      if (strncmp(cmd, ":fetc?", 6) == 0 ){
+        //:FETCh?
+        if(precteno==2 || precteno==3){ Serial.print(aktualniNastaveni.columnsSeparator);Serial.print(' ');}
+        Serial.println(stableDutyCyclePromile);
+        if (strlen(cmd) > 7) { cmd += 7; precteno = 2;}
+        else precteno = 4;
+      }
+      if (strncmp(cmd, ":measure:pwidth?",16) == 0 ){
+        //:MEASure:PWIDth?
+        pwid = (float)impuls / 1000000.0;
+        if(precteno==2 || precteno==3){Serial.print(aktualniNastaveni.columnsSeparator);Serial.print(' ');}
+        tiskniFloat(pwid,6,aktualniNastaveni.decimalSeparator);
+        if (strlen(cmd) > 17) {cmd += 17; precteno = 2;}
+        else precteno = 4;
+      }
+      if (strncmp(cmd, ":meas:pwid?", 11) == 0 ){
+        //:MEASure:PWIDth?
+        pwid = (float)impuls / 1000000.0;
+        if(precteno==2 || precteno==3){Serial.print(aktualniNastaveni.columnsSeparator);Serial.print(' ');}
+        tiskniFloat(pwid,6,aktualniNastaveni.decimalSeparator);
+        if (strlen(cmd) > 12) {cmd += 12; precteno = 2;}
+        else precteno = 4;
+      }
+      if (strncmp(cmd, ":measure:period?", 16) == 0 ){
+        //MEASure:PERiod?
+        if(precteno==2 || precteno==3){Serial.print(aktualniNastaveni.columnsSeparator);Serial.print(' ');}
+        tiskniFloat(stablePeriod / 1000000.0,6,aktualniNastaveni.decimalSeparator);
+        if (strlen(cmd) > 17) {cmd += 17; precteno = 2;}
+        else precteno = 4;
+      }
+      if (strncmp(cmd, ":meas:per?", 10) == 0 ){
+        //MEASure:PERiod?
+        if(precteno==2 || precteno==3){Serial.print(aktualniNastaveni.columnsSeparator);Serial.print(' ');}
+        tiskniFloat(stablePeriod / 1000000.0,6,aktualniNastaveni.decimalSeparator);
+        if (strlen(cmd) > 11) {cmd += 11; precteno = 2;}
+        else precteno = 4;
+      }
+      if (strncmp(cmd, "-te", 3) == 0) {
+        if (! zpracujUniverzalniPrikaz(cmd, 100, 65000, nullptr, nullptr, &aktualniNastaveni.t_error,  nullptr)){
+          tiskniProgmem(TEXT_TE);
+        }; precteno = 255;
+      } 
+      if (strncmp(cmd, "-t ", 3) == 0) {
+        if (! zpracujUniverzalniPrikaz(cmd, 1, 99, &aktualniNastaveni.spodni_hranice, &aktualniNastaveni.horni_hranice)){
+          tiskniProgmem(TEXT_T);
+        }; precteno = 255;
+      } 
+      if (strncmp(cmd, "-i", 2) == 0) {
+        if (! zpracujUniverzalniPrikaz(cmd, 0, 1, &aktualniNastaveni.input, nullptr, nullptr, nullptr)){
+          tiskniProgmem(TEXT_I);
+        }; precteno = 255;
+      } 
+      if (strncmp(cmd, "-l", 2) == 0) {
+        if (! zpracujUniverzalniPrikaz(cmd, 0, 1, &aktualniNastaveni.listing, nullptr, nullptr, nullptr)){
+          tiskniProgmem(TEXT_L);
+        }; precteno = 255;
+      }
+      if (strncmp(cmd, "-p", 2) == 0) {
+        if (! zpracujUniverzalniPrikaz(cmd, 100, 65000, nullptr, nullptr, &aktualniNastaveni.min_perioda, &aktualniNastaveni.max_perioda)){
+          tiskniProgmem(TEXT_P);
+        }; precteno = true;
+      } 
+      if (strncmp(cmd, "-s", 2) == 0) {
+        if (! zpracujUniverzalniPrikaz(cmd, 1, 499, nullptr, nullptr, &aktualniNastaveni.min_strida, &aktualniNastaveni.max_strida)){
+          tiskniProgmem(TEXT_S);
+        }; precteno = 255;
+      } 
+      if (strncmp(cmd, "-e", 2) == 0) {
+        if (! zpracujUniverzalniPrikaz(cmd, 0, 255, &aktualniNastaveni.max_error, nullptr, nullptr, nullptr)){
+          tiskniProgmem(TEXT_E);
+        }; precteno = 255;
+      } 
+      if (strncmp(cmd, "-b", 2) == 0) {
+        if (! zpracujUniverzalniPrikaz(cmd, 96, 1152, nullptr, nullptr, &aktualniNastaveni.bps,  nullptr)){
+          tiskniProgmem(TEXT_BPS);
+        }; precteno = 255;
+      } 
+      if (strncmp(cmd, "-ds", 3) == 0) {
+        if(strlen(cmd) == 5) {
+          char znak = cmd[4];
+          aktualniNastaveni.decimalSeparator=znak;
+          EEPROM.put(EEPROM_ADRESA, aktualniNastaveni);
+          Serial.println(F("\nLimits successfully updated:"));
+          zobrazNastaveni();
+          precteno = 255;
+        }
+      } 
+
+      if (strncmp(cmd, "-cs", 3) == 0) {
+        if(strlen(cmd) == 5) {
+          char znak = cmd[4];
+          aktualniNastaveni.columnsSeparator=znak;
+          EEPROM.put(EEPROM_ADRESA, aktualniNastaveni);
+          Serial.println(F("\nLimits successfully updated:"));
+          zobrazNastaveni();
+          precteno = 255;
+        }
+      }
+      
+      if (precteno==4){
+        //
+        Serial.print("\n");
+        precteno=255;
+      }
+      if (precteno==0 || precteno==3 ){
+        Serial.print(F("\nUnknown command or wrong format: "));
+        Serial.println(cmd);
+        tiskniProgmem(TEXT_H);
+        precteno=255;
+      }
+      if (precteno == 2) precteno=3;
+
     }
+    
+
 
     bufferIndex = 0; // Reset index pro další příkaz
     newData = false;
@@ -478,4 +546,26 @@ void loop() {
   #ifdef DEBUG_MODE
     delay(100);
   #endif
+}
+
+void tiskniFloat(float cislo, int pocetDesetinnChyb, char separator) {
+    // 1. Převod čísla na String pomocí standardní tečky
+    String s = String(cislo, pocetDesetinnChyb);
+
+    // 2. Najdi pozici desetinné tečky
+    int teckaIndex = s.indexOf('.');
+
+    if (teckaIndex != -1) {
+        // 3. Rozděl řetězec na celou a desetinnou část
+        String celaCast = s.substring(0, teckaIndex);
+        String desetinnaCast = s.substring(teckaIndex + 1);
+
+        // 4. Vytiskni s čárkou
+        Serial.print(celaCast);
+        Serial.print(separator); // Tady je čárka!
+        Serial.print(desetinnaCast);
+    } else {
+        // Pokud číslo neobsahuje desetinnou část (je celé), vytiskni ho normálně
+        Serial.print(s);
+    }
 }
